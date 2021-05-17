3e5d21ed0979602d4358c4ed73cfedda89da531f
raajeev.ranjann <raajeev.ranjann@gmail.com>
2021-05-17 14:50:32 +0530

    error middleware created

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,26 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
-
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 connectDB();
 const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.originalUrl);
+  next();
+});
 app.get("/", (req, res) => {
   res.send("API is running.....");
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
